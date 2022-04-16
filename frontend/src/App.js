58860391307c3347ff3261b49524ff02891d3d01
mohitagrawal939 <mohitagrawal939@gmail.com>
2022-04-17 01:33:58 +0530

    product details with router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header/>
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
